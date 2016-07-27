615c355e5d4e8fb6b5c64c4218fb0cfb26d28b55
enod <enod2008@gmail.com>
2016-07-27 18:58:24 +0800

    "bug fix on regex - instead of . will do [өүу] "

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ with codecs.open(directory + "/output.txt", "r", encoding='utf-8') as f:
 def replacer(word):
     search_word = translit(word, 'mn')
     if u"ө" in search_word:
-        search_word_regex = search_word.replace(u"ө", ".")
+        search_word_regex = search_word.replace(u"ө", u"[өүу]")
         search_word_regex_reform = "^" + search_word_regex + '$'
         found_or_not = [item for i, item in enumerate(data) if re.search(search_word_regex_reform, item)]
 
@@ -30,7 +30,7 @@ def replacer(word):
         else:
             return search_word
     elif u"у" in search_word:
-        search_word_regex = search_word.replace(u"у", ".")
+        search_word_regex = search_word.replace(u"у", u"[өүу]")
         search_word_regex_reform = "^" + search_word_regex + '$'
         found_or_not = [item for i, item in enumerate(data) if re.search(search_word_regex_reform, item)]
 
@@ -41,7 +41,7 @@ def replacer(word):
     elif u'үү' == search_word:
         return u'уу'
     elif u"ү" in search_word:
-        search_word_regex = search_word.replace(u"ү", ".")
+        search_word_regex = search_word.replace(u"ү", u"[өүу]")
         search_word_regex_reform = "^"+search_word_regex+'$'
         found_or_not = [item for i, item in enumerate(data) if re.search(search_word_regex_reform, item)]
 
